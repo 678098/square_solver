3ba8077140dd8c39ec0a448c45a376f04b2f59af
e_malygin <678098@protonmail.com>
2021-06-10 21:42:36 +0300

    init cpp build with cmake

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main(int argc, char **argv) {
+
+    int value;
+    while (std::cin >> value) {
+        std::cout << value << std::endl;
+    }
+    
+    return 0;
+}

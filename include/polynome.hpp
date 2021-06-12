4c9ad1691de95b1db218c92e09b49de3c8b4d448
e_malygin <678098@protonmail.com>
2021-06-13 01:22:43 +0300

    add utility methods for polynome
    refactor ostream output code

--- include/polynome.hpp
+++ include/polynome.hpp
@@ -1,28 +1,82 @@
 #pragma once
 
 #include <vector>
+#include <cassert>
 
 
 template<typename ValueType>
 class Polynome
 {
 public:
-    Polynome(const std::vector<ValueType> &coeffs): coeffs(coeffs) {
+    Polynome(const std::vector<ValueType> &coeffs):
+        kCoeffs(coeffs),
+        kDegree(findDegree(coeffs))
+    {
     }
     virtual ~Polynome() = default;
     
+    int degree() const {
+        return kDegree;
+    }
     
-    std::vector<ValueType> coeffs;
+    const std::vector<ValueType> &coeffs() const {
+        return kCoeffs;
+    }
     
-    friend std::ostream &operator<<(std::ostream &out, const Polynome &poly) {
-        out << "(";
-        for (int i = int(poly.coeffs.size()) - 1; i >= 1; i--) {
-            out << poly.coeffs[i] << " ";
+    size_t size() const {
+        return kCoeffs.size();
+    }
+    
+    const ValueType &operator[] (size_t index) const {
+        assert(index < kCoeffs.size());
+        return kCoeffs[index];
+    }
+    
+    const ValueType &a() const {
+        assert(kCoeffs.size() >= 3);
+        return kCoeffs[2];
+    }
+    
+    const ValueType &b() const {
+        assert(kCoeffs.size() >= 2);
+        return kCoeffs[1];
+    }
+    
+    const ValueType &c() const {
+        assert(kCoeffs.size() >= 1);
+        return kCoeffs[0];
+    }
+    
+private:
+
+    const int kDegree;
+    const std::vector<ValueType> kCoeffs;
+    
+    static bool isAlmostZero(const ValueType &value) {
+        constexpr double kEPS = 1e-6;
+        return -kEPS < value && value < kEPS;
+    }
+    
+    static int findDegree(const std::vector<ValueType> &coeffs) {
+        for (size_t i = 0; i < coeffs.size(); i++) {
+            if (!isAlmostZero(coeffs[i])) {
+                return coeffs.size() - i;
+            }
         }
-        if (!poly.coeffs.empty()) {
-            out << poly.coeffs[0];
-        }
-        out << ")";
-        return out;
+        return 0;
     }
 };
+
+
+template<typename ValueType>
+std::ostream &operator<<(std::ostream &out, const Polynome<ValueType> &poly) {
+    out << "(";
+    for (int i = int(poly.size()) - 1; i >= 1; i--) {
+        out << poly[i] << " ";
+    }
+    if (!poly.coeffs.empty()) {
+        out << poly[0];
+    }
+    out << ")";
+    return out;
+}
--- include/solver.hpp
+++ include/solver.hpp
@@ -18,6 +18,23 @@ template<typename ValueType>
 using EquationRoots = std::variant<std::vector<ValueType>, SpecificEquationRoot>;
 
 
+std::ostream &operator<<(std::ostream &out, const SpecificEquationRoot &root) {
+    switch (root) {
+        case SpecificEquationRoot::kNoRoots:
+            out << "no roots";
+            break;
+        case SpecificEquationRoot::kAnyNumberIsRoot:
+            out << "any number";
+            break;
+        case SpecificEquationRoot::kCannotSolve:
+            out << "cannot solve";
+            break;
+        default:
+            throw std::runtime_error("Unexpected SpecificEquationRoot found");
+    }
+    return out;
+}
+
 template<typename ValueType>
 std::ostream &operator<<(std::ostream &out, const EquationRoots<ValueType> &roots) {
     std::size_t index = roots.index();
@@ -32,20 +49,7 @@ std::ostream &operator<<(std::ostream &out, const EquationRoots<ValueType> &root
         }
         out << ")";
     } else {
-        SpecificEquationRoot rt = std::get<SpecificEquationRoot>(roots);
-        switch (rt) {
-            case SpecificEquationRoot::kNoRoots:
-                out << "no roots";
-                break;
-            case SpecificEquationRoot::kAnyNumberIsRoot:
-                out << "any number";
-                break;
-            case SpecificEquationRoot::kCannotSolve:
-                out << "cannot solve";
-                break;
-            default:
-                throw std::runtime_error("Unexpected SpecificEquationRoot found");
-        }
+        out << std::get<SpecificEquationRoot>(roots);
     }
     return out;
 }
@@ -56,27 +60,27 @@ class EquationSolver
 {
 public:    
     EquationRoots<ValueType> solve(const Polynome<ValueType> &polynome) const {
-        const ValueType &a = polynome.coeffs[2];
-        const ValueType &b = polynome.coeffs[1];
-        const ValueType &c = polynome.coeffs[0];
+        const ValueType &a = polynome.a();
+        const ValueType &b = polynome.b();
+        const ValueType &c = polynome.c();
         
-        if (std::abs(a) < 1e-6) {
+        if (polynome.degree() < 2) {
             //todo solve
             return SpecificEquationRoot::kCannotSolve;
         }
         
-        ValueType d = b*b - 4 * a * c;
+        ValueType d = b * b - 4 * a * c;
         if (d < 0) {
             return SpecificEquationRoot::kNoRoots;
         }
         
         ValueType sqrt_d = std::sqrt(d);
         //todo optimize
-        ValueType x1 = (-b + sqrt_d) / (2 * a);
-        ValueType x2 = (-b - sqrt_d) / (2 * a);
+        ValueType a2 = 2 * a;
+        ValueType x1 = (-b + sqrt_d) / a2;
+        ValueType x2 = (-b - sqrt_d) / a2;
         
         return std::vector<ValueType>({x1, x2});
     }
-    
     
 };
